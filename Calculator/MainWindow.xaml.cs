41e6b27bff25a7463b87f768cbfe07688a358fe6
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-07-21 20:34:01 +0300

    Finished UI and cleaned the code

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +15,243 @@ namespace Calculator
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private const string one = "1";
+        private const string two = "2";
+        private const string three = "3";
+        private const string four = "4";
+        private const string five = "5";
+        private const string six = "6";
+        private const string seven = "7";
+        private const string eight = "8";
+        private const string nine = "9";
+
+        private const string multiply = "*";
+        private const string divide = "/";
+        private const string percentage = "%";
+        private const string minus = "-";
+        private const string plus = "+";
+
         public MainWindow()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //KEYBOARD ACTIONS
+
+        //arithmetics and others
+        private void DeleteBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
         {
+
+        }
+        
+        private void DivideBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void MultiplyBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void MinusBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void PlusBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void DecimalBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void RemoveBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void EqualsBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+
+        //numbers
+        private void ZeroBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void OneBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void TwoBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void ThreeBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void FourBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void FiveBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void SixBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void SeventBtn_Click_Witk_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void EightBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+        private void NineBtn_Click_With_Key(object sender, KeyboardAcceleratorInvokedEventArgs e)
+        {
+
+        }
+
+
+        //MOUSE ACTIONS
+
+        //arithmetics and others
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBlock.Text = "0";
+        }
+
+        private void NegateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (outputTextBlock.Text != "0")
+            {
+                string outputText = outputTextBlock.Text;
+
+                if (outputText.Contains('/') ||
+                    outputText.Contains('*') ||
+                    outputText.Contains('+') ||
+                    outputText.Contains('-') &&
+                    outputText.Contains(' '))
+                {
+                    var temp = outputText
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToArray();
+
+                    int lastNumIndex = temp.Length - 1;
+
+                    if (double.Parse(temp[lastNumIndex]) > 0)
+                    {
+                        temp[lastNumIndex] = $"-{temp[lastNumIndex]}";
+                    }
+                    else
+                    {
+                        temp[lastNumIndex] = temp[lastNumIndex].Remove(0, 1);
+                    }
+
+                    outputTextBlock.Text = String.Join(" ", temp);
+                }
+                else
+                {
+                    if (double.Parse(outputText) > 0)
+                    {
+                        outputTextBlock.Text = $"-{outputText}";
+                    }
+                    else
+                    {
+                        outputTextBlock.Text = outputText.Remove(0, 1);
+                    }
+                }
+            }
+        }
+
+        private void DecimalBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (!outputTextBlock.Text.EndsWith(' '))
+            {
+                if (outputTextBlock.Text.EndsWith('.'))
+                {
+
+                }
+                else
+                {
+                    outputTextBlock.Text += ".";
+                }
+            }
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = outputTextBlock.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            int lastIndex = temp.Length - 1;
+
+            temp[lastIndex] = temp[lastIndex].Remove(temp[lastIndex].Length - 1, 1);
+
+            outputTextBlock.Text = String.Join(" ", temp);
+        }
+
+        private void EqualsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: make a service class with the bussiness logic
             outputTextBlock.Text = "Result";
+        }
+
+        private void ArithmeticBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            string content = btn.Content.ToString();
+
+            if (outputTextBlock.Text.EndsWith(" "))
+            {
+
+            }
+            else
+            {
+                outputTextBlock.Text += $" {content} ";
+            }
+        }
+
+
+        //numbers
+        private void NumberBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            string content = btn.Content.ToString();
+
+            double result;
+
+            if (double.TryParse(content, out result))
+            {
+                if (outputTextBlock.Text == "0")
+                {
+                    outputTextBlock.Text = content;
+                }
+                else
+                {
+                    outputTextBlock.Text += content;
+                }
+            }
         }
     }
 }
