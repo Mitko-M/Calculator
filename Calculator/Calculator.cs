7063687e9d4fcbbda4b2ac302598cb5c24ee6ace
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-07-22 01:27:16 +0300

    Added Calculator class

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.Security.Authentication.Identity.Provider;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        private string input;
+        private char[] artihmetics = { '*', '/', '-', '+' };
+
+        public Calculator()
+        {
+            
+        }
+
+        public Calculator(string _input)
+        {
+            input = _input;
+        }
+
+        public string Calculate(string calculatorInput = null)
+        {
+            string result = "";
+
+           //TODO: think of the business logic and implement it then use the class in the windows.cs
+
+            return result;
+        }
+    }
+}
